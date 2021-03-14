6a5acd14714cd938d218e0601ebb14f493889d78
udimberto <udi@ingresse.com>
2021-03-14 04:47:06 -0300

    Updates

---
+++ precache-manifest.eb89406674062c3accfa1b44b510b3d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db86ba03320f55bc9d34b10a1595db98",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fb07cf1f1e1b24668072",
+    "url": "./static/css/main.7986e143.chunk.css"
+  },
+  {
+    "revision": "4fff20e5e46df99451c0",
+    "url": "./static/js/2.c8466e56.chunk.js"
+  },
+  {
+    "revision": "86e0fe6a98f1001a109810c483bfd35c",
+    "url": "./static/js/2.c8466e56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb07cf1f1e1b24668072",
+    "url": "./static/js/main.474d142f.chunk.js"
+  },
+  {
+    "revision": "ed8ed6512420da37da10",
+    "url": "./static/js/runtime-main.49c4df90.js"
+  }
+]);
