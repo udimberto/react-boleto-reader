f3715929fd4069988e5bfd98a4938c30c947c091
udimberto <udi@ingresse.com>
2021-03-15 17:21:13 -0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.ecad813d943feeacf5ba443257d2047a.js"
+  "./precache-manifest.7fc93d0ae4c0ddc657b2ab7243549f15.js"
 );
 
 self.addEventListener('message', (event) => {
